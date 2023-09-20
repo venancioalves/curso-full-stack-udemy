013d3076e798513608f198889dfd84b102a7bab0
venancioalves <venaciobsn@gmail.com>
2023-09-20 17:00:35 -0300

    Aula 31

--- secao 4/primeiro-app/src/App.js
+++ secao 4/primeiro-app/src/App.js
@@ -4,61 +4,42 @@ import { useState} from 'react';
 
 function App (){
 
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [idade, setIdade] = useState(0);
-
-  const [user, setUser] = useState({});
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Primeira tarefa',
+    'Segunda tarefa'
+  ]);
 
   function handleRegister(e){
     e.preventDefault();
 
-    alert('Usuario registrado com sucesso!')
-
-    setUser({
-      nome: nome,
-      idade: idade,
-      email: email,
-    })
+    setTarefas([...tarefas, input]);
+    setInput('');
 
   }
 
   return(
     <div>
       <form onSubmit={handleRegister}>
-        <h1>Cadastrando usuario</h1><br/>
-
-        <label>Nome: </label><br/>
+        
+        <label>Nome da tarefa: </label><br/>
         <input 
-        placeholder='Digite seu nome'
-        value={nome}
-        onChange={ (e) => setNome(e.target.value)}
+        placeholder='Digite uma tarefa'
+        value={input}
+        onChange={ (e) => setInput(e.target.value)}
         /><br/>
 
-        <label>Email: </label><br/>
-        <input placeholder='Digite seu email '
-        value={email}
-        onChange={ (e) => setEmail(e.target.value)}
-        /><br/>
-
-        <label>Idade: </label><br/>
-        <input placeholder='Digite sua idade '
-        value={idade}
-        onChange={ (e) => setIdade(e.target.value)}
-        /><br/>
 
         <button type='submit'>Registrar</button>
       </form>
 
       <br/><br/>
 
-      <div>
-        <span>Bem vindo: {user.nome} </span><br/>
-        <span>Email:     {user.email}</span><br/>
-        <span>Idade:     {user.idade}</span>
-
-
-      </div>
+      <ul>
+        {tarefas.map( tarefa => (
+          <li key={tarefa}>{tarefa}</li>
+        ))}
+      </ul>
 
 
     </div>
