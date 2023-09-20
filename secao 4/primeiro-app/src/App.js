dfcb12adbc51e7b820d7f1407fb523c07f3db304
venancioalves <venaciobsn@gmail.com>
2023-09-20 14:12:25 -0300

    Aula 29

--- secao 4/primeiro-app/src/App.js
+++ secao 4/primeiro-app/src/App.js
@@ -1,11 +1,24 @@
+import { useState} from 'react';
+
 import Nome from './components/Nome';
 
 function App (){
+
+  const [aluno, setAluno] = useState('Sujeito')
+
+  function handleChangeName (nome){
+    setAluno(nome);
+  }
+
   return(
     <div>
-      <h1 >Componente App</h1>
-      <nome aluno="Venâncio Alves" idade={23} />
-      <Nome/>
+      <h1 >Componente App</h1><br/>
+      <h2>Olá: {aluno}</h2>
+      <button onClick={() => handleChangeName('Marylia')}>
+        Mudar Nome
+      </button>
+      
+      
     </div>
   );
 }
